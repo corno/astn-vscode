ab83bed9ccf3578d6c78f3a098b4615ca3c38e2c
corno <gitkraken.a@schraverus.com>
2021-06-13 04:06:17 +0200

    u2l

--- client/src/astnWrappers/index.ts
+++ client/src/astnWrappers/index.ts
@@ -3,30 +3,33 @@ import * as p20 from 'pareto-20'
 import * as p from "pareto"
 
 export function format(
-	documentContent: string,
-	replace: (range: astn.Range, newValue: string) => void,
-	del: (range: astn.Range) => void,
-	insert: (location: astn.Location, newValue: string) => void,
+	_documentContent: string,
+	_replace: (range: astn.Range, newValue: string) => void,
+	_del: (range: astn.Range) => void,
+	_insert: (location: astn.Location, newValue: string) => void,
 ) {
-	const formatter = astn.createFormatter(
-		"    ",
-		replace,
-		del,
-		insert,
-		() => {
-			return p.value(null)
-		}
-	)
-	const parserStack = astn.createParserStack(
-		() => {
-			return formatter
-		},
-		() => {
-			return formatter
-		}
-	)
-	return p20.createArray([documentContent]).streamify().tryToConsume(
-		null,
-		parserStack
-	).convertToNativePromise(() => "something went wrong")
+	// const formatter = astn.createFormatter(
+	// 	"    ",
+	// 	replace,
+	// 	del,
+	// 	insert,
+	// 	() => {
+	// 		return p.value(null)
+	// 	}
+	// )
+	// const parserStack = astn.createParserStack(
+	// 	() => {
+	// 		return formatter
+	// 	},
+	// 	() => {
+	// 		return formatter
+	// 	}
+	// )
+	// return p20.createArray([documentContent]).streamify().tryToConsume(
+	// 	null,
+	// 	parserStack
+	// ).convertToNativePromise(() => "something went wrong")
+	return new Promise(x => {
+		x(null)
+	})
 }
